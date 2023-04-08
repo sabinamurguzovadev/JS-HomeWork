be345aca967514ab8c93df3147f4997bdd385b20
sabinamurguzovadev <sabinamurguzova2003@gmail.com>
2023-04-08 23:33:45 +0400

    js first task added

---
+++ script.js
@@ -0,0 +1,63 @@
+// task 1
+// let age = 65
+// let days = age * 365
+// console.log(days);
+
+// task 2
+// let word = "Front-end Developer"
+// let output = ""
+// for (let i = 0; i < word.length;i++){
+//     output += word[i]
+//     console.log(output);
+// }
+
+// task 3
+// for (let i = 0; i < 10; i++){
+//     if (i % 2 ===0 && i !==10) {
+//         console.log(i);
+
+//     }
+
+// }
+
+// task 4
+// let result = 1
+// for (let i = 1; i <= 17;i+=2){
+//     if(i !==17){
+//         result *=i
+//     }
+
+// }
+// console.log(result);
+// task 5
+// let sum = 0
+// for (i = 17;i<=30;i++ ){
+//     if (i !==19 && i !==20){
+//         sum +=i
+        
+//     }
+// }
+// console.log(sum);
+
+// task 6
+// let totalPair = 138
+// let totalOdd = 161
+// while (totalOdd>=totalPair){
+//     if(totalOdd===totalPair){
+//         totalOdd=0
+//     } else{
+//         totalOdd--
+//     }
+   
+
+// }
+
+// task 7
+// for (i = 0 ; i<= 16;i++){
+//     if(i%2===0 ) {
+//     console.log(Math.pow(i,2));
+
+        
+//     }
+// }
+
